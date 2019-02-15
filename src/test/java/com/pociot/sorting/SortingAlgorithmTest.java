f906c9b8a54fdbca93f561d8fad495de7cc54980
kamilpociot <kamil.pociot@me.com>
2019-02-15 20:57:34 +0100

    ci fix

--- src/test/java/com/pociot/sorting/SortingAlgorithmTest.java
+++ src/test/java/com/pociot/sorting/SortingAlgorithmTest.java
@@ -2,7 +2,7 @@ package com.pociot.sorting;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 
 public class SortingAlgorithmTest extends SortingTestBase {
 
@@ -11,57 +11,55 @@ public class SortingAlgorithmTest extends SortingTestBase {
 
   private static final String LONG_RUNNING_TESTS = "long-running-tests";
 
-  private static final String CI = "CI";
-
   @BeforeEach
   void setUp() {
     setUp(TEST_DATA_FILE, TEST_DATA_SORTED_FILE);
   }
 
   @Test
-  @EnabledIfSystemProperty(named = LONG_RUNNING_TESTS, matches = "true")
+  @EnabledIfEnvironmentVariable(named = LONG_RUNNING_TESTS, matches = "true")
   void insertionSortTest() {
     sortTest(new InsertionSort());
   }
 
   @Test
-  @EnabledIfSystemProperty(named = LONG_RUNNING_TESTS, matches = "true")
+  @EnabledIfEnvironmentVariable(named = LONG_RUNNING_TESTS, matches = "true")
   void selectionSortTest() {
     sortTest(new SelectionSort());
   }
 
   @Test
-  @EnabledIfSystemProperty(named = LONG_RUNNING_TESTS, matches = "true")
+  @EnabledIfEnvironmentVariable(named = LONG_RUNNING_TESTS, matches = "true")
   void mergeSortTest() {
     sortTest(new MergeSort());
   }
 
   @Test
-  @EnabledIfSystemProperty(named = LONG_RUNNING_TESTS, matches = "true")
+  @EnabledIfEnvironmentVariable(named = LONG_RUNNING_TESTS, matches = "true")
   void heapSortTest() {
     sortTest(new HeapSort());
   }
 
   @Test
-  @EnabledIfSystemProperty(named = LONG_RUNNING_TESTS, matches = "true")
+  @EnabledIfEnvironmentVariable(named = LONG_RUNNING_TESTS, matches = "true")
   void quickSortTest() {
     sortTest(new QuickSort());
   }
 
   @Test
-  @EnabledIfSystemProperty(named = LONG_RUNNING_TESTS, matches = "true")
+  @EnabledIfEnvironmentVariable(named = LONG_RUNNING_TESTS, matches = "true")
   void bubbleSortTest() {
     sortTest(new BubbleSort());
   }
 
   @Test
-  @EnabledIfSystemProperty(named = LONG_RUNNING_TESTS, matches = "true")
+  @EnabledIfEnvironmentVariable(named = LONG_RUNNING_TESTS, matches = "true")
   void shellSortTest() {
     sortTest(new ShellSort());
   }
 
   @Test
-  @EnabledIfSystemProperty(named = LONG_RUNNING_TESTS, matches = "true")
+  @EnabledIfEnvironmentVariable(named = LONG_RUNNING_TESTS, matches = "true")
   void combSortTest() {
     sortTest(new CombSort());
   }
